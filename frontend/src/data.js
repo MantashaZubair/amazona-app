cf8435de2b49e32c23f7e2dfad50d5caf1ea3260
MantashaZubair <zmantasha3@gmail.com>
2022-06-24 15:23:03 -0700

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            name:"Nike Slim Shirt",
+            catagory:"shirt",
+            image:"/images/p1.jpg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        },
+        {
+            _id:'2',
+            name:"Adidas Slim Shirt",
+            catagory:"shirt",
+            image:"/images/p2.jpg",
+            price:110,
+            brand:"Adidas",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        },
+        {
+            _id:'3',
+            name:"Polo Slim Shirt",
+            catagory:"shirt",
+            image:"/images/p3.jpg",
+            price:99,
+            brand:"Polo",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        },
+        {
+            _id:'4',
+            name:"Nike Fit Pant",
+            catagory:"pants",
+            image:"/images/p4.jpg",
+            price:87,
+            brand:"Nike",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        },
+        {
+            _id:'5',
+            name:"Puma Fit Pant",
+            catagory:"pant",
+            image:"/images/p5.jpg",
+            price:90,
+            brand:"Puma",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        },
+        {
+            _id:'6',
+            name:"Adidas Fit pant",
+            catagory:"pant",
+            image:"/images/p6.jpg",
+            price:78,
+            brand:"Adidas",
+            rating:4.5,
+            numReviwe:10,
+            discription:"high quality product"
+        }
+    ]
+}
+export default data;
